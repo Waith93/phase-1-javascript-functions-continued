8e5a0228afef3801f352ca4d333949f7aa437dcc
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-05 14:03:01 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// Function: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function: mondayWork
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  // Function: wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage:
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a hard worker")); // Output: "You are !!!a hard worker!!!"
+  
